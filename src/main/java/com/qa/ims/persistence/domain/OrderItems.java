74403fcdee5f85516331b19d37ff6c8ba1096295
MuhammadShayekurRahman <shayek.rahman@gmail.com>
2022-06-07 12:40:52 +0100

    Completed OrderItems domain

--- src/main/java/com/qa/ims/persistence/domain/OrderItems.java
+++ src/main/java/com/qa/ims/persistence/domain/OrderItems.java
@@ -1,5 +1,85 @@
 package com.qa.ims.persistence.domain;
 
+import java.util.Objects;
+
 public class OrderItems {
+	
+	private Long orderItemsId;
+	private Long quantity;
+	private Order order;
+	private Items items;
+	
+	public OrderItems(Long orderItemsId, Long quantity, Order order, Items items) {
+		super();
+		this.orderItemsId = orderItemsId;
+		this.quantity = quantity;
+		this.order = order;
+		this.items = items;
+	}
+
+	public OrderItems(Long quantity, Order order, Items items) {
+		super();
+		this.quantity = quantity;
+		this.order = order;
+		this.items = items;
+	}
+
+	public Long getOrderItemsId() {
+		return orderItemsId;
+	}
+
+	public void setOrderItemsId(Long orderItemsId) {
+		this.orderItemsId = orderItemsId;
+	}
+
+	public Long getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Long quantity) {
+		this.quantity = quantity;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	public Items getItems() {
+		return items;
+	}
+
+	public void setItems(Items items) {
+		this.items = items;
+	}
+
+	@Override
+	public String toString() {
+		return "OrderItems [orderItemsId=" + orderItemsId + ", quantity=" + quantity + ", order=" + order + ", items="
+				+ items + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(items, order, orderItemsId, quantity);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItems other = (OrderItems) obj;
+		return Objects.equals(items, other.items) && Objects.equals(order, other.order)
+				&& Objects.equals(orderItemsId, other.orderItemsId) && Objects.equals(quantity, other.quantity);
+	}
+	
+	
 
 }
